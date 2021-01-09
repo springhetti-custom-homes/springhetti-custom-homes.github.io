a476097ea63c09bbf0cd3a4db95d8793473d71f0
AJ Sakher <ajsakher@gmail.com>
2021-01-08 18:57:27 -0600

    Update styles

--- assets/js/gallery_image_display.js
+++ assets/js/gallery_image_display.js
@@ -59,5 +59,51 @@ const displayImages = () => {
     }
 };
 
+window.addEventListener("load", () => {
+    let displayPhotos = document.getElementById("display-photos");
+
+    displayPhotos.addEventListener("touchstart", handleTouchStart, false);
+    displayPhotos.addEventListener("touchmove", handleTouchMove, false);
+});
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+            prevImage();
+        } else {
+            /* right swipe */
+            nextImage();
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
+
 updateShownImage();
 displayImages();
